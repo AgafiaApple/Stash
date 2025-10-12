63ecd1f33e0fb871ecf0cea7b14c97026c510988
Agafia Apple <agafiabowden@gmail.com>
2025-10-12 17:07:32 -0500

    finished implementing loadContacts and saveContacts
    finished implementing getSize()
    
    in ContactList.kt

--- app/src/main/java/com/example/sharingapp/ContactList.kt
+++ app/src/main/java/com/example/sharingapp/ContactList.kt
@@ -11,17 +11,14 @@ import java.io.IOException
 import java.io.ObjectInputStream
 import java.io.ObjectOutputStream
 
-class ContactList(contacts : ArrayList<Contact>, FILENAME : String) {
-
-    var contacts = contacts
-    var FILENAME = FILENAME
+class ContactList(var contacts: ArrayList<Contact>?, var FILENAME: String) {
 
 
     fun setContacts(contact_list : ArrayList<Contact>) {
         this.contacts = contact_list
     }
 
-    fun getContacts() : ArrayList<Contact>{
+    fun getContacts() : ArrayList<Contact>?{
         return this.contacts
     }
 
@@ -42,7 +39,13 @@ class ContactList(contacts : ArrayList<Contact>, FILENAME : String) {
     }
 
     fun getSize() : Int {
-
+        if (this.contacts == null) {
+            return 0
+        } else {
+            // !! is used as a non-null assertion and will
+                // throw a NullPointerException if this.contacts is null
+            return this.contacts!!.size
+        }
     }
 
     // checks this.contacts
@@ -62,11 +65,23 @@ class ContactList(contacts : ArrayList<Contact>, FILENAME : String) {
 
     // loads the contacts from a file into this.contacts
     fun loadContacts(context : Context) {
+        val contactsFile = getContactsFile(context)
 
+        // 1. Open the file for reading
+        //      ObjectInputStream reads the object from the file
+        //      `use` ensures the file is closed immediately after use
+        this.contacts = ObjectInputStream(FileInputStream(contactsFile)).use {
+            // 2. Read the object from the file
+            //      objectInputStream.readObject() reads the object that was written to the file
+            //
+            objectInputStream -> objectInputStream.readObject() as ArrayList<Contact>?
+        }
 
-
-
-    }
+        if (this.contacts == null) {
+            println("Warning! `this.contacts` was loaded as `null`. This means either the user has no" +
+                    " contacts yet or that an error occurred.")
+        }
+    } // end loadContacts()
 
     private fun getContactsFile(context : Context) : File {
         val contactsFile = File(context.filesDir, "contacts")
@@ -91,15 +106,28 @@ class ContactList(contacts : ArrayList<Contact>, FILENAME : String) {
 
     } // end getContactsFile
 
-    // saves the this.contacts in a file in ArrayList format
+    // saves the this.contacts in a file in ArrayList<Contact> format
     fun saveContacts(context : Context) {
+        val contactsFile = getContactsFile(context)
 
-
-    }
+        ObjectOutputStream(FileOutputStream(contactsFile)).use {
+            objectOutputStream -> objectOutputStream.writeObject(this.contacts)
+        }
+    } // end saveContacts
 
 
     fun isUsernameAvailable(username : String) : Boolean {
+        val iter = this.contacts?.listIterator()
 
-    }
+        // enter while loop if iter is NOT null
+        while(iter?.hasNext() ?: false) {
+            val contact = iter.next()
+            if (contact.getUsername().equals(username)) {
+                return false
+            }
+        }
+
+        return true
+    } // end isUsernameAvailable
 
 } // end ContactList class
