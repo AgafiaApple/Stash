40d974d10f33befa5d2892e131496fb007eac620
Agafia Apple <agafiabowden@gmail.com>
2025-10-12 20:32:51 -0500

    fixed typos in AvailableItemsFragment.kt

--- app/src/main/java/com/example/sharingapp/AvailableItemsFragment.kt
+++ app/src/main/java/com/example/sharingapp/AvailableItemsFragment.kt
@@ -19,14 +19,14 @@ class AvailableItemsFragment : ItemsFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val rootView = inflater.inflate(R.layout.borrowed_items_fragment, container, false) // Initialize rootView
-        super.setVariables(R.layout.available_items_fragment, R.id.my_available_items)
+        val rootView = inflater.inflate(R.layout.fragment_borrowed_items, container, false) // Initialize rootView
+        super.setVariables(R.layout.fragment_available_items, R.id.my_available_items)
         super.setAdapter(this) // Changed AvailableItemsFragment.this to this
 
         return rootView
     }
 
-    fun filterItems(): ArrayList<Item> {
+    override fun filterItems(): ArrayList<Item> {
         val status = "Available"
         return item_list.filterItemsByStatus(status)
     }
