21df4ef44400f3e2dc99ec19699f86449453b2b7
Agafia Apple <agafiabowden@gmail.com>
2025-11-10 11:58:20 -0600

    build successful after adding more dependencies from the old project build.gradle.kts (:app) file. I commented some out for now because they caused errors, and I am not sure if they are necessary.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,13 +56,35 @@ android {
 dependencies {
     // this line is necessarry for the Jsonify interface
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
-    
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // added
+    // errors occurred, so I commented these out for now
+//    androidTestImplementation(platform(libs.androidx.compose.bom))
+//    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+//    debugImplementation(libs.androidx.compose.ui.tooling)
+//    debugImplementation(libs.androidx.compose.ui.test.manifest)
+//    implementation(libs.androidx.fragment.ktx)
+
+    implementation(libs.androidx.appcompat)
+
+    implementation(libs.androidx.appcompat)
+    implementation("com.google.android.material:material:1.13.0")
+    implementation("androidx.coordinatorlayout:coordinatorlayout:1.3.0")
+    implementation("androidx.viewpager:viewpager:1.1.0")
+
+    // below dependencies are for FireStore database usage
+    implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
+    implementation("com.google.firebase:firebase-analytics")
+
+    // needed for reading and writing json files
+    implementation("com.google.code.gson:gson:2.13.2")
 
     implementation(libs.androidx.runtime)
     implementation(libs.androidx.runtime.livedata)
